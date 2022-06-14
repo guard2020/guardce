8da5e2edba9ec1a46d049db1833209029d2df258
guard2020 <104438564+guard2020@users.noreply.github.com>
2022-06-14 11:34:23 +0200

    Update build-volumes.sh

--- guard_cloud/volumes/build-volumes.sh
+++ guard_cloud/volumes/build-volumes.sh
@@ -10,6 +10,8 @@ mkdir -p $1/data/zookeeper/data
 mkdir -p $1/data/zookeeper/datalog
 
 mkdir -p $1/data/kafka1/data
+mkdir -p $1/data/kafka2/data
+mkdir -p $1/data/kafka3/data
 
 mkdir -p $1/logstash-guard/config
 cp ../logstash-guard/config/* $1/logstash-guard/config
@@ -18,6 +20,8 @@ cp ../logstash-guard/pipeline/* $1/logstash-guard/pipeline
 mkdir -p $1/logstash-cb/file-output
 
 mkdir -p $1/elastic/data01
+mkdir -p $1/elastic/data02
+mkdir -p $1/elastic/data03
 mkdir -p $1/security_dashboard/guard_docker/run/var
 
 mkdir -p $1/alert
