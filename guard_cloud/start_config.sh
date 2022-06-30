17bbc9f7cb6399a83bfacb97f2a6542b243ae86f
guard2020 <104438564+guard2020@users.noreply.github.com>
2022-06-30 16:03:02 +0200

    Update start_config.sh

--- guard_cloud/start_config.sh
+++ guard_cloud/start_config.sh
@@ -17,11 +17,11 @@ curl  -v -X POST  http://$1:5000/type/exec-env  -H 'Content-Type: application/js
     
 curl  -v -X POST  http://$1:5000/catalog/algorithm  -H 'Content-Type: application/json' -d @./cb-manager/cnit.json
 
-curl -v -X POST -H "Content-Type: application/json" http://$1:4100/self/configuration -d @./cb-manager/guard-agent.json
+#curl -v -X POST -H "Content-Type: application/json" http://$1:4100/self/configuration -d @./cb-manager/guard-agent.json
 
-curl -v -X POST -H "Content-Type: application/json" http://$1:4100/agent/type -d @./cb-manager/catalog-guard-agent.json
+#curl -v -X POST -H "Content-Type: application/json" http://$1:4100/agent/type -d @./cb-manager/catalog-guard-agent.json
 
-curl -v -X POST -H "Content-Type: application/json" http://$1:4100/agent/instance -d @./cb-manager/instance-guard-agent.json
+#curl -v -X POST -H "Content-Type: application/json" http://$1:4100/agent/instance -d @./cb-manager/instance-guard-agent.json
 
 curl -v -X POST -H "Content-Type: application/json" http://$1:5000/type/network-link -d @./cb-manager/network-type.json
 
