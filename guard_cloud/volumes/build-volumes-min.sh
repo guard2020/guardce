724ec12e9416aa343f741d42e4435ef17e4b759c
guard2020 <104438564+guard2020@users.noreply.github.com>
2022-07-11 14:17:52 +0200

    Update build-volumes-min.sh

--- guard_cloud/volumes/build-volumes-min.sh
+++ guard_cloud/volumes/build-volumes-min.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
-### usage ./build-volumes-min.sh VOLUME_DIR  
-
-if [ "$#" -ne 1 ]; then
-        echo "Usage: ./Usage build-volumes-min.sh VOLUME_DIR "
-        exit 1
-    fi    
+### usage ./build-volumes-min.sh 
 
 export $(cat ../.env | xargs)
 
-mkdir -p $1/data/zookeeper/data
-chmod 777 $1/data/zookeeper/data
-mkdir -p $1/data/zookeeper/datalog
-chmod 777 $1/data/zookeeper/datalog
-mkdir -p $1/data/kafka1/data
-chmod 777 $1/data/kafka1/data
+mkdir -p $VOLUME_DIR/data/zookeeper/data
+chmod 777 $VOLUME_DIR/data/zookeeper/data
+mkdir -p $VOLUME_DIR/data/zookeeper/datalog
+chmod 777 $VOLUME_DIR/data/zookeeper/datalog
+mkdir -p $VOLUME_DIR/data/kafka1/data
+chmod 777 $VOLUME_DIR/data/kafka1/data
 
-mkdir -p $1/logstash-guard/config
-cp ../logstash-guard/config/* $1/logstash-guard/config
-mkdir -p $1/logstash-guard/pipeline
-cp ../logstash-guard/pipeline/* $1/logstash-guard/pipeline
-mkdir -p $1/logstash-cb/file-output
-chmod 777 $1/logstash-cb/file-output
+mkdir -p $VOLUME_DIR/logstash-guard/config
+cp ../logstash-guard/config/* $VOLUME_DIR/logstash-guard/config
+mkdir -p $VOLUME_DIR/logstash-guard/pipeline
+cp ../logstash-guard/pipeline/* $VOLUME_DIR/logstash-guard/pipeline
+mkdir -p $VOLUME_DIR/logstash-cb/file-output
+chmod 777 $VOLUME_DIR/logstash-cb/file-output
 
-mkdir -p $1/elastic/data01
-chmod 777 $1/elastic/data01
+mkdir -p $VOLUME_DIR/elastic/data01
+chmod 777 $VOLUME_DIR/elastic/data01
 
-mkdir -p $1/alert
+mkdir -p $VOLUME_DIR/alert
 sed  -i -e "s/hosts:X/hosts: $GUARD_SERVER_ADDRESS:$ELASTIC_PORT_1/" ../alert/config.yaml
-cp ../alert/config.yaml $1/alert 
+cp ../alert/config.yaml $VOLUME_DIR/alert 
 
-mkdir -p $1/logdata-anomaly-miner/source/root/etc/aminer/conf-enabled
-cp ../Aminer/config.yml $1/logdata-anomaly-miner/source/root/etc/aminer
-cp ../Aminer/ApacheAccessModel.py $1/logdata-anomaly-miner/source/root/etc/aminer/conf-enabled
+mkdir -p $VOLUME_DIR/logdata-anomaly-miner/source/root/etc/aminer/conf-enabled
+cp ../Aminer/config.yml $VOLUME_DIR/logdata-anomaly-miner/source/root/etc/aminer
+cp ../Aminer/ApacheAccessModel.py $VOLUME_DIR/logdata-anomaly-miner/source/root/etc/aminer/conf-enabled
 
-mkdir -p $1/scan_reports
-chmod 777 $1/scan_reports
+mkdir -p $VOLUME_DIR/scan_reports
+chmod 777 $VOLUME_DIR/scan_reports
 
 chmod 666 /var/run/docker.sock
 exit 0
