0d1cdac130a35f2c4ab3fdeb7eea180dc4e071a0
guard2020 <104438564+guard2020@users.noreply.github.com>
2022-07-11 11:56:22 +0200

    Update start_ini.sh

--- guard_cloud/start_ini.sh
+++ guard_cloud/start_ini.sh
@@ -1,2 +1,2 @@
 export $(cat ../.env | xargs)
-bash cb-manager/start_config.sh $GUARD_SERVER_ADDRESS $CB_MAN_PORT $LCP_PORT $ELASTIC_PORT_1  
+bash cb-manager/start_config.sh $GUARD_SERVER_ADDRESS $CB_MAN_PORT $LCP_PORT $ELASTIC_PORT_1 $KIBANA_PORT 
