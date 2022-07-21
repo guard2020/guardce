f04adae8163aa38466d081dc26fe1ac34dddd123
guard2020 <104438564+guard2020@users.noreply.github.com>
2022-07-21 15:17:37 +0200

    Update gen_cert.sh

--- guard_cloud/volumes/gen_cert.sh
+++ guard_cloud/volumes/gen_cert.sh
@@ -19,18 +19,6 @@ openssl x509 -req -CA ca-cert -CAkey ca-key -in kafka3-cert-sign-request -out ka
   -extfile <(cat /tmp/openssl.cnf \
     <(printf "\n[SAN]\nsubjectAltName=DNS:$1,DNS:kafka1,DNS:kafka2,DNS:kafka3,IP:$2")) 
 
-keytool -genkey -keystore idp.jks -validity 365 -storepass "wso2carbon" -keypass "wso2carbon" -dname "CN=$1" -storetype jks -keyalg RSA -keysize 4096
-
-keytool -keystore idp.jks -certreq -file idp-cert-sign-request -storepass "wso2carbon" -keypass "wso2carbon" 
-cp /etc/ssl/openssl.conf /tmp
-openssl x509 -req -CA ca-cert -CAkey ca-key -in idp-cert-sign-request -out idp-cert-signed -days 365 -CAcreateserial -passin pass:"keytool -keystore idp.jks -certreq -file idp-cert-sign-request -storepass "wso2carbon" -keypass "wso2carbon" 
-"  -extensions SAN \
-  -extfile <(cat /tmp/openssl.cnf \
-    <(printf "\n[SAN]\nsubjectAltName=DNS:$1,DNS:idp,DNS:wso2carbon,DNS:kafka3,IP:$2\nextendedKeyUsage=serverAuth,clientAuth")) 
-
-keytool -keystore idp.jks -alias CARoot -import -file ca-cert -storepass "wso2carbon" -keypass "wso2carbon" -noprompt
-keytool -keystore idp.jks -import -file idp-cert-signed -storepass "wso2carbon" -keypass "wso2carbon"  -noprompt
-
 
 
 keytool -keystore kafka.server.truststore.jks -alias CARoot -import -file ca-cert -storepass "password" -keypass "password" -noprompt
@@ -40,3 +28,20 @@ keytool -keystore kafka3.server.keystore.jks -alias CARoot -import -file ca-cert
 keytool -keystore kafka1.server.keystore.jks -import -file kafka1-cert-signed -storepass "password" -keypass "password"  -noprompt
 keytool -keystore kafka2.server.keystore.jks -import -file kafka2-cert-signed -storepass "password" -keypass "password"  -noprompt
 keytool -keystore kafka3.server.keystore.jks -import -file kafka3-cert-signed -storepass "password" -keypass "password"  -noprompt
+
+
+### IDP ###
+keytool -genkey -alias wso2carbon -keystore idp.jks -validity 365 -storepass "wso2carbon" -keypass "wso2carbon" -dname "CN=wso2carbon" -storetype jks -keyalg RSA -keysize 4096
+
+keytool -keystore idp.jks -alias wso2carbon -certreq -file idp-cert-sign-request -storepass "wso2carbon" -keypass "wso2carbon" 
+
+cp /etc/ssl/openssl.cnf /tmp
+
+openssl x509 -req -CA ca-cert -CAkey ca-key -in idp-cert-sign-request -out idp-cert-signed -days 365 -CAcreateserial -passin pass:"keytool -keystore idp.jks -certreq -file idp-cert-sign-request -storepass "wso2carbon" -keypass "wso2carbon" 
+"  -extensions SAN \
+  -extfile <(cat /tmp/openssl.cnf \
+    <(printf "\n[SAN]\nsubjectAltName=DNS:$1,DNS:idp,DNS:wso2carbon,IP:$2\nextendedKeyUsage=serverAuth,clientAuth")) 
+
+keytool -keystore idp.jks -alias CARoot -import -file ca-cert -storepass "wso2carbon" -keypass "wso2carbon" -noprompt
+keytool -keystore idp.jks -alias wso2carbon -import -file idp-cert-signed -storepass "wso2carbon" -keypass "wso2carbon"  -noprompt
+
