559a73551b2c1605bc53ba32df8822daa2385142
guard2020 <104438564+guard2020@users.noreply.github.com>
2022-10-03 10:32:41 +0200

    Update start_config.sh

--- guard_cloud/cb-manager/start_config.sh
+++ guard_cloud/cb-manager/start_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#### Usage start_config.sh internal_ip_address cb-managet-port lcp-port es-port kibana-port
+#### Usage start_config.sh internal_ip_address cb-manager-port lcp-port es-port kibana-port
 
 if [ "$#" -ne 5 ]; then
-        echo "Usage: ./start_config.sh internal_ip_address cb-managet-port lcp-port es-port kibana-port"
+        echo "Usage: ./start_config.sh internal_ip_address cb-manager-port lcp-port es-port kibana-port"
         exit 1
 fi
 
